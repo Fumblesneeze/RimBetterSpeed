a521558025df379308b86918dafb2d3c0986d89d
Fumblesneeze <Fumblesneeze@github.com>
2020-03-28 22:06:32 +0100

    Hotfix

--- Source/BetterSpeed/Compatibility_GiddyUp.cs
+++ Source/BetterSpeed/Compatibility_GiddyUp.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.AI;
 
 namespace BetterSpeed
 {
@@ -32,9 +33,11 @@ namespace BetterSpeed
                     var paramExpr = Expression.Parameter(typeof(Pawn), "mount");
                     var jobDriverExpr = Expression.Field(Expression.Field(paramExpr, nameof(Pawn.jobs)), "curDriver");
                     var conditionExpr = Expression.Condition(
-                            Expression.TypeIs(jobDriverExpr, jobDriverType),
-                                Expression.Property(Expression.Convert(jobDriverExpr, jobDriverType), "Rider"),
-                                Expression.Default(typeof(Pawn)));
+                            Expression.AndAlso(
+                                Expression.NotEqual(Expression.Field(paramExpr, nameof(Pawn.jobs)), Expression.Default(typeof(Pawn_JobTracker))),
+                                Expression.TypeIs(jobDriverExpr, jobDriverType)),
+                            Expression.Property(Expression.Convert(jobDriverExpr, jobDriverType), "Rider"),
+                            Expression.Default(typeof(Pawn)));
                     GetRider = Expression.Lambda<Func<Pawn, Pawn>>(conditionExpr, paramExpr).Compile();
                     Log.Message($"BetterSpeed: GiddyUp Compatibility! (2/2)");
                 }
